c29f67ad3f50cffdfe5ca7a5764573d89a0abed7
phpBBEesti <argol76@gmail.com>
2018-12-20 08:30:52 +0200

    Update info_acp_topic_prefixes.php

--- language/et/info_acp_topic_prefixes.php
+++ language/et/info_acp_topic_prefixes.php
@@ -2,6 +2,7 @@
 /**
  *
  * Topic Prefixes extension for the phpBB Forum Software package.
+ * Estonian translation by phpBBestonia.eu <https://www.phpbbestonia.eu>
  *
  * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
@@ -22,10 +23,10 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_TOPIC_PREFIXES'	=> 'Topic prefixes',
-	'ACP_MANAGE_PREFIXES'	=> 'Manage prefixes',
+	'ACP_TOPIC_PREFIXES'	=> 'Teemade kategooriad',
+	'ACP_MANAGE_PREFIXES'	=> 'Halda kategooriaid',
 
 	// ACP Log messages
-	'ACP_LOG_PREFIX_ADDED'		=> '<strong>Created new topic prefix</strong><br />» %1$s in forum: %2$s',
-	'ACP_LOG_PREFIX_DELETED'	=> '<strong>Deleted topic prefix</strong><br />» %1$s in forum: %2$s',
+	'ACP_LOG_PREFIX_ADDED'		=> '<strong>Lisa uus teema kategooria</strong><br />» %1$s foorumis: %2$s',
+	'ACP_LOG_PREFIX_DELETED'	=> '<strong>Kustuta teema kategooria</strong><br />» %1$s foorumis: %2$s',
 ));
