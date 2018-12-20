e589d8b4a475e39e86103a514048ef6d92fd013a
phpBBEesti <argol76@gmail.com>
2018-12-20 08:28:43 +0200

    Update topic_prefixes.php

--- language/et/topic_prefixes.php
+++ language/et/topic_prefixes.php
@@ -2,6 +2,7 @@
 /**
  *
  * Topic Prefixes extension for the phpBB Forum Software package.
+ * Estonian translation by phpBBestonia.eu <https://www.phpbbestonia.eu>
  *
  * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
